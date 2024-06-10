e50ffcd5c213d8878500267884f962797f3f6758
Niraj Ramakant Chaudhari <nirajchaudhari8698@gmail.com>
2024-06-10 11:18:06 -0700

    Reposition Down arrow based on screen width mobile or not

--- src/components/sections/ContactIcons.jsx
+++ src/components/sections/ContactIcons.jsx
@@ -25,10 +25,14 @@ export const ContactIcons = (props) => {
   return (
     <div className="w-fit">
       <div className={`flex flex-row mt-4 w-fit ${injectClasses}`}>
-        <a href="tel:+12135619960">
+        <a
+          href="https://api.whatsapp.com/send?phone=2135619960"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <motion.img
-            alt="Phone"
-            src={callIcon}
+            alt="WhatsApp"
+            src={whatsAppIcon}
             className="object-contain"
             style={{
               width: iconScaling,
@@ -72,14 +76,10 @@ export const ContactIcons = (props) => {
             variants={vibrateVariants}
           />
         </a>
-        <a
-          href="https://api.whatsapp.com/send?phone=2135619960"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="tel:+12135619960">
           <motion.img
-            alt="WhatsApp"
-            src={whatsAppIcon}
+            alt="Phone"
+            src={callIcon}
             className="object-contain"
             style={{
               width: iconScaling,
@@ -89,7 +89,7 @@ export const ContactIcons = (props) => {
             variants={vibrateVariants}
           />
         </a>
-        {isMobile && (
+        {window.innerWidth < 768 && (
           <svg className="arrows">
             <path className="a1" d="M0 0 L20 24 L40 0"></path>
             <path className="a2" d="M0 14 L20 38 L40 14"></path>
@@ -97,8 +97,8 @@ export const ContactIcons = (props) => {
           </svg>
         )}
       </div>
-      {!isMobile && (
-        <svg className="arrows" style={{ marginTop: "25px", margin: "auto" }}>
+      {window.innerWidth >= 768 && (
+        <svg className="arrows" style={{ margin: "auto", marginTop: "25px" }}>
           <path className="a1" d="M0 0 L20 24 L40 0"></path>
           <path className="a2" d="M0 14 L20 38 L40 14"></path>
           <path className="a3" d="M0 28 L20 48 L40 28"></path>
