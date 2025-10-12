941e9061911ec0f2a87394a3e4a5c0ebd6bdacd6
Niraj Ramakant Chaudhari <nirajchaudhari8698@gmail.com>
2025-10-11 23:47:36 -0700

    Update SkillsSection.jsx

--- src/components/sections/SkillsSection.jsx
+++ src/components/sections/SkillsSection.jsx
@@ -192,7 +192,7 @@ export const SkillsSection = (props) => {
       className={`${isMobile ? "h-[60vh]" : ""}`}
     >
       <h2 className="text-2xl md:text-5xl font-bold bg-purple-700 p-2 rounded text-white sectionHeading w-fit mt-4">
-        What's in my Mind ?
+        Technical Skills
       </h2>
 
       <AnimatePresence>
