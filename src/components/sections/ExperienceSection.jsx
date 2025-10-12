22c0060ce354c5a1f68b1464200c64ab946a7c8e
Niraj Ramakant Chaudhari <nirajchaudhari8698@gmail.com>
2025-10-11 22:12:17 -0700

    Update ExperienceSection.jsx

--- src/components/sections/ExperienceSection.jsx
+++ src/components/sections/ExperienceSection.jsx
@@ -2,7 +2,7 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
-import usc from "../../assets/logo/usc.png";
+import paypal from "../../assets/logo/paypal.svg"
 import infosys from "../../assets/logo/infosys.jpg";
 import proxel from "../../assets/logo/proxel.png";
 import "react-vertical-timeline-component/style.min.css";
